00cb9afe98d55425b32759972452ddc506e481ce
venugopal <hegdev@simplot.com>
2019-10-03 02:14:46 +0530

    Rewact hooks basics

--- react-burger/src/App.js
+++ react-burger/src/App.js
@@ -1,19 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
-class App extends Component {
-  render() {
+
+const app = props =>{
+ const [personsState, setPersonsState]= useState({
+    persons : [
+      {name: "Venu" , age:"234"},
+      {name:"gopal", age:"2132"},
+      {name:"hegde", age:"999"}
+    ]
+  })
+
+  const switchNmaeHandler = ()=>{
+    //console.log("was clicked")
+    setPersonsState({persons: [
+      {name: "Venu" , age:"234"},
+      {name:"gopaksdkksdkkl", age:"2132"},
+      {name:"hegde", age:"999"}
+    ]})
+  }
     return (
       <div className="App">
         <h1> Hi Iam react developer</h1>
-        <Person name="venu" age="132"/>
-        <Person name="gopal" age="123423"/>
-        <Person name="hegde" age="999">Iah fine</Person>
-        <Person name="llll" age="2323"/>
+        <button onClick={switchNmaeHandler}> Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>Iah fine</Person>
+        
       </div>
     );
   //  return React.createElement('div',{className : 'App'},React.createElement('h1', null, "Hii is it working?"));
   }
-}
 
-export default App;
+export default app;
+
+
+
