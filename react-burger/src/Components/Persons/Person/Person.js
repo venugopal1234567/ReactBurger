59b3037b47ae89394f175b46c217e79cee43efff
venugopal <hegdev@simplot.com>
2019-10-06 12:54:54 +0530

    Props LifeCycle

--- react-burger/src/Components/Persons/Person/Person.js
+++ react-burger/src/Components/Persons/Person/Person.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React,{Component} from 'react'
 import classes from './Person.css'
-const person = (props)=>{
+
+
+class Person extends Component{
+
+    render(){
+        return (
+            <div className={classes.Person} >
+                  
+                  <p onClick={this.props.click}> Iam {this.props.name} react developer with {this.props.age}</p>
+                 <p>{this.props.children}</p> 
+                 <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+            
+        )
+    }
     
-    return (
-        <div className={classes.Person} >
-              
-              <p onClick={props.click}> Iam {props.name} react developer with {props.age}</p>
-             <p>{props.children}</p> 
-             <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-        
-    )
 }
 
-export default person;
+export default Person;
--- react-burger/src/Components/Persons/Persons.js
+++ react-burger/src/Components/Persons/Persons.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React,{Component} from 'react'
 
 import Person from './Person/Person'
 
-const persons = (props)=> props.persons.map((person ,index) =>{
-    return  <Person
-     click={()=> props.clicked(index)}
-     name={person.name} 
-     age={person.age} 
-     key={person.id}
-     changed= {(event)=>props.changed(event,person.id)}
-     />
-  })
+class Persons extends Component{
+    // static getDerivedStateFromProps(props, state){
+    //     console.log('[Person.js] getDerivedStateFromProps')
+    //     return state;
+    // }
 
-  export default persons;
+    shouldComponentUpdate(nextProps, nextState){
+        console.log('[Person.js] Should COmponent Update')
+        return true;
+    }
+   
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('[Person.js] getSnapShotBeforeUpdate')
+        return null;
+    }
+
+    componentDidUpdate(){
+        console.log('[Person.js]componentDidUpdate')
+    }
+ render(){
+    console.log('[Person.js] Render')
+     return  this.props.persons.map((person ,index) =>{
+        return  <Person
+         click={()=> this.props.clicked(index)}
+         name={person.name} 
+         age={person.age} 
+         key={person.id}
+         changed= {(event)=>this.props.changed(event,person.id)}
+         />
+      })
+ }
+}
+
+  export default Persons;
